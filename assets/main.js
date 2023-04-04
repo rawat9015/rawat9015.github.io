238bbbf65f14042b1324a079d873af6e25b16a22
rawat9015 <nr7584128@gmail.com>
2023-04-04 20:53:10 +0500

    responsiveness

---
+++ assets/main.js
@@ -0,0 +1,43 @@
+// <!-- scroll top button  -->
+
+    var button = document.getElementById("btn1");
+
+    function scrollfun() {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+
+            button.style.display = "block";
+        }
+
+        else {
+            button.style.display = "none";
+        }
+
+    }
+    window.onscroll = function () { scrollfun() };
+    function topfunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
+
+
+// <!-- banner animtion -->
+
+    const text = document.querySelector(".sec-text");
+
+    const wordbreak = () => {
+        setTimeout(() => {
+            text.textContent = "Coder"
+        }, 0);
+
+        setTimeout(() => {
+            text.textContent = "Web Designer"
+        }, 4000);
+
+        setTimeout(() => {
+            text.textContent = "Web Developer"
+        }, 8000);
+    }
+    wordbreak();
+    setInterval(wordbreak, 12000);
+
